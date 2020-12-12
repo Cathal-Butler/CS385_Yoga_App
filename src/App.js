20d7cb26dd854bdb75493c58955d2b69dbd2c617
Cathal-Butler <73025090+Cathal-Butler@users.noreply.github.com>
2020-12-12 13:40:27 +0000

    Beginner.js file created Created Beginner.js file in components folder and imported in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Beginner from "./components/Beginner";
 import Intermediate from "./components/Intermediate";
 import Advanced from "./components/Advanced";
 import "./styles.css";
@@ -19,6 +20,7 @@ class App extends Component {
       apiDataBeginner: [],
       apiDataIntermediate: [],
       apiDataAdvanced: [],
+      // level: {isBeginner: false, isIntermediate: false, isAdvanced: false},
       isFetched: false,
       errorMsg: null,
       isBeginner: false,
@@ -186,42 +188,6 @@ class App extends Component {
 } // end of App class
 
 //***************************Beginner Mapping Content Component****************************//
-class Beginner extends Component {
-  render() {
-    //this const declaration connects this Beginner class to the App class. It is the way to pass the//
-    //apiDataBeginner state to call the map function on it from within this component//
-    const mapBeginner = this.props.mapObjectBeginner;
-
-    return (
-      <div className="card-group">
-        {mapBeginner.map((person, index) => (
-          <div className="card text-center">
-            <div class="card">
-              <div className="card-body">
-                <img
-                  className="card-img-top"
-                  alt="yogapic"
-                  src={person.imgURL}
-                  key={index}
-                />
-                <br />
-                <br />
-                <h3 className="card-title">{person.body_part}</h3>
-                <h5 className="car-title">{person.position}</h5>
-                <p className="card-text">{person.description}</p>
-
-                <audio controls autoplay>
-                  <source src={person.audio} />
-                </audio>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
-//**************End of Beginner Component**********************//
 
 //**************Start of Intermediate Mapping Content Component***************//
 // The intermediate Component is now in its own file in the components folder and is imported into the App//
