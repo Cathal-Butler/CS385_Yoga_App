af41c04be2b7b3c423cfda425eed08afbb62e63f
jphoulihan <39299247+jphoulihan@users.noreply.github.com>
2020-12-18 18:17:28 +0000

    initial commit

--- src/components/Custom.js
+++ src/components/Custom.js
@@ -6,25 +6,21 @@ class Custom extends Component {
     //apiDataBeginner state to call the map function on it from within this component//
     const mapCustom = this.props.mapObjectCustom;
     return (
-      <div className="card-group">
-        {mapCustom.map((person, index) => (
+      <div className="card-deck">
+        {mapCustom.map((person) => (
           <div className="card text-center">
-            <div class="card">
-              <div className="card-body">
-                <img
-                  className="card-img-top"
-                  alt="yogapic"
-                  src={person.imgURL}
-                  key={index}
-                />
-                <br />
-                <br />
-                <h3 className="card-title">{person.body_part}</h3>
-                <h5 className="car-title">{person.position}</h5>
-                <p className="card-text">{person.description}</p>
+            <img
+              className="card-img-top"
+              alt="yogapic"
+              src={person.imgURL}
+              key={person.id}
+            />
+            <div class="card-body d-flex flex-column">
+              <h3 className="card-title">{person.body_part}</h3>
+              <h5 className="car-title">{person.position}</h5>
+              <p className="card-text">{person.description}</p>
 
-                <ReactAudioPlayer src={person.audio} autoplay controls />
-              </div>
+              <ReactAudioPlayer src={person.audio} autoplay controls />
             </div>
           </div>
         ))}
